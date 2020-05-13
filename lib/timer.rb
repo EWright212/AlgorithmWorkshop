7cea9a1884bae78cffe0c3121cc825cd6caec2dc
Student <emilywright212@gmail.com>
2020-05-13 10:37:28 +0100

    Refactor - remove unused code

--- lib/timer.rb
+++ lib/timer.rb
@@ -1,24 +1,11 @@
 class Timer
 
-  attr_reader :array
-  
-  @variable
-
-  @array = 
-
   def get_time(function)
     start_time = Time.now
     array = [1, 2, 3, 4, 4, 5, 6, 7, 7, 8, 3, 5435, 45, 1, 543, 4, 6, 6]
     p array.last
     total_time = Time.now - start_time
     return total_time
-  end
-
-end
-
-class Functions
-  
-  def this
   end
 
 end
