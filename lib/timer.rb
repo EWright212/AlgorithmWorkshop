295c05a57a73a1c5e0c14e8f808783806ac01d52
Student <emilywright212@gmail.com>
2020-05-13 10:39:44 +0100

    array is localised variable

--- lib/timer.rb
+++ lib/timer.rb
@@ -1,9 +1,12 @@
 class Timer
 
+  def initialize
+    @array = [1, 2, 3, 4, 4, 5, 6, 7, 7, 8, 3, 5435, 45, 1, 543, 4, 6, 6]
+  end
+
   def get_time(function)
     start_time = Time.now
-    array = [1, 2, 3, 4, 4, 5, 6, 7, 7, 8, 3, 5435, 45, 1, 543, 4, 6, 6]
-    p array.last
+    @array.last
     total_time = Time.now - start_time
     return total_time
   end
